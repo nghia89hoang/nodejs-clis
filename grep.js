9032b2d99374d2b71c1d32ba4ac6603343319bd9
Nghe-HP <nghia89hoang@gmail.com>
2016-10-16 16:03:20 +0700

    + Add grep

--- grep.js
+++ grep.js
@@ -1,14 +1,32 @@
-#!/usr/bin/env/ babel-node
+#!/usr/bin/env babel-node
 require('./helper')
 
 const fs = require('fs')
+const readLine = require('readline')
 
 const argv = process.argv
 const searchFor = argv[2]
 const searchPath = argv[3]
 
 async function grep(param, fileName) {
-  
+  const rl = readLine.createInterface({
+    input: fs.createReadStream(fileName)
+  })
+  const results = []
+  const lineCallback = (line) => {
+    const a = (l, r) => {
+      if (l.indexOf(param) !== -1) {
+        results.push(l)
+      }
+    }
+    return a(line, results)
+  }
+  rl.on('line', lineCallback)
+  await rl.promise.on('close')
+  return results
+}
+async function main() {
+  (await grep(searchFor, searchPath)).forEach((item) => console.log(item))
 }
 
-grep(searchFor, searchPath)
+main()
